8beafa3b1f83b83a38c8777f0e5cc944ce17755d
Vladimir Voloshinov <vladimir.voloshinov@gmail.com>
2021-06-10 01:23:50 +0300

    Tru to reproduce Maxima examples. TBC

---
+++ vvvTest/testMesh2d.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+def FF(y1, y2):
+    return  -2*(y1/5)**3+3*(y1/3-y2/7)**2+3*(y2/9)**2
+
+Y1 = np.arange(0, 4, 0.01)
+Y2 = np.arange(0, 4, 0.01)
+Y1, Y2 = np.meshgrid(Y1, Y2)
+# R = np.sqrt(X**2 + Y**2)
+Z = FF(Y1,Y2)
+
+# Plot the surface.
+surf = ax.plot_surface(Y1, Y2, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+# Customize the z axis.
+ax.set_zlim(0, 4.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
+
+(N1, N2) = (4, 4) # Number of points in 2d meshgrid
+# print(N1, ' ', N2)
+(d1, d2) = (1, 1)
+y1 = [d1*i for i in range(0,N1+1)]
+y2 = [d1*j for j in range(0,N2+1)]
+
+A = np.ndarray(shape=(N1+1, N2+1))
+for j in range(N2+1):
+    for i in range(N1+1):
+        A[i,j] = ( (FF(y1[i], y2[j]) - FF(y1[i-1], y2[j]))/d1 if i > 0 else 0)
+
+print(A.transpose())
