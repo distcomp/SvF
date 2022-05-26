97ddbaf62f989e6f74a8dd82bd0f80c4f3bf3d05
Vladimir Voloshinov <vladimir.voloshinov@gmail.com>
2022-05-27 00:09:45 +0300

    initXtFy & addOde1

--- test/testPW.py
+++ test/testPW.py
@@ -6,7 +6,7 @@ dx(t)/dt = F(x(t))
 d2x(t)/d2t = F(x(t),x'(t))
 """
 
-def addOde1(model: pyo.ConcreteModel, setT: list, yMin: float, yMax: float, Ny: int, eps: float, useEta = True):
+def addOde1x(model: pyo.ConcreteModel, setT: list, yMin: float, yMax: float, Ny: int, eps: float, useEta = True):
     Nx = len(setT)-1
     model.setTidx = pyo.RangeSet(0,Nx)
 
@@ -84,19 +84,136 @@ def addOde1(model: pyo.ConcreteModel, setT: list, yMin: float, yMax: float, Ny: 
     else:
         model.ODE1_Sqrt = pyo.Constraint(model.setOdeK, rule=ODE1_Sqrt_rule)
 
+def addOde1(model: pyo.ConcreteModel, meshT, varXt, Nx: int, meshY, varFy, yMin: float, yMax: float, Ny: int, eps: float, useEta = True):
+    dy = (yMax - yMin) / Ny
+    def A(m, j: int):
+        return (varFy[j] - varFy[j-1])/dy
+
+    def D2F(m, j: int):
+        # (A(j)-A(j-1))/dy
+        return (varFy[j] - 2*varFy[j-1] + varFy[j-2])/dy
+
+    def cntrX(m, k: int):
+        return (varXt[k] + varXt[k-1])/2.
+
+    model.setOdeK = pyo.RangeSet(1, Nx)
+    if useEta:
+        # model.setEtaK = pyo.RangeSet(1, Nt - 1)
+        model.setEtaJ = pyo.RangeSet(1, Ny - 1)
+        model.Eta = pyo.Var(model.setOdeK, model.setEtaJ, within=pyo.PositiveReals)
+
+    # def Eta_rule(m, k, j):
+    #     return (m.Eta[k, j]**2 == (m.Xt[k] - m.meshY[j])**2 + eps**2)
+    def Eta_rule(m, k, j):
+        return (m.Eta[k, j]**2 == (cntrX(m, k) - m.meshY[j])**2 + eps**2)
+    if useEta:
+        model.Eta_constr = pyo.Constraint(model.setOdeK, model.setEtaJ, rule=Eta_rule)
+
+    # def ODE1_Eta_rule(m, k):
+    #     return ((m.Xt[k+1] - m.Xt[k])/(m.meshT[k+1] - m.meshT[k]) ==
+    #              m.Fy[0] - A(m, 1)*yMin + (1./2.)*(A(m, 1) + A(m, Ny))*m.Xt[k] +
+    #             (1. / 2.)*sum(D2F(m, j)*(m.Eta[k, j-1] - m.meshY[j-1]) for j in range(2, Ny))
+    #             )
+    def ODE1_Eta_rule(m, k):
+        return ((varXt[k] - varXt[k-1])/(meshT[k] - meshT[k-1]) ==
+                 varFy[0] - A(m, 1)*yMin + (1./2.)*(A(m, 1) + A(m, Ny))*cntrX(m, k) +
+                (1. / 2.)*sum(D2F(m, j)*(m.Eta[k, j-1] - meshY[j-1]) for j in range(2, Ny))
+                )
+
+    # def ODE1_Sqrt_rule(m, k):
+    #     return ((m.Xt[k+1] - m.Xt[k])/(m.meshT[k+1] - m.meshT[k]) ==
+    #              m.Fy[0] - A(m, 1)*yMin + (1./2.)*(A(m, 1) + A(m, Ny))*m.Xt[k] +
+    #             (1. / 2.)*sum(D2F(m, j)*(pyo.sqrt((m.Xt[k] - m.meshY[j])**2 + eps**2) - m.meshY[j-1]) for j in range(2, Ny))
+    #             )
+    def ODE1_Sqrt_rule(m, k):
+        return ((varXt[k] - varXt[k-1])/(meshT[k] - meshT[k-1]) ==
+                 varFy[0] - A(m, 1)*yMin + (1./2.)*(A(m, 1) + A(m, Ny))*cntrX(m, k) +
+                (1. / 2.)*sum(D2F(m, j)*(pyo.sqrt((cntrX(m, k) - meshY[j])**2 + eps**2) - meshY[j-1]) for j in range(2, Ny))
+                )
+
+    if useEta:
+        model.ODE1_Eta = pyo.Constraint(model.setOdeK, rule=ODE1_Eta_rule)
+    else:
+        model.ODE1_Sqrt = pyo.Constraint(model.setOdeK, rule=ODE1_Sqrt_rule)
+
+def initXtFy(model, setT, yLo, yUp, Ny):
+    Nx = len(setT) - 1
+    model.setTidx = pyo.RangeSet(0, Nx)
+
+    def meshT_init(m, k):
+        return setT[k]
+    model.meshT = pyo.Param(model.setTidx, initialize=meshT_init)
+
+    # Create y-meshgrid
+    model.setYidx = pyo.RangeSet(0, Ny)
+    model.dy = pyo.Param(initialize=(yUp - yLo) / Ny)
+    dy = (yUp - yLo) / Ny
+
+    def meshY_init(m, i):
+        return yLo + i * dy
+    model.meshY = pyo.Param(model.setYidx, initialize=meshY_init)
+
+    model.Fy = pyo.Var(model.setYidx, within=pyo.Reals)
+    model.Xt = pyo.Var(model.setTidx, within=pyo.Reals)
+
+    def printData():
+        print("||||||||||| DATA |||||||||||||||")
+        print("Nx = ", Nx, " Ny = ", Ny)
+        print("meshT: ", [t for t in setT])
+        print("meshY: ", [y for y in theModel.meshY])
+        print("||||||||||||||||||||||||||||||||")
+
+    printData()
+
+
 
 if __name__ == "__main__":
+
     theModel = pyo.ConcreteModel("test Ode1_Eta")
-    meshT = [i*2 for i in range(3)]
+
+    Nx = 20
+    setT = [i*2 for i in range(Nx+1)]
+    Ny = 40
     yLo = 55.
     yUp = 67.
-    Ny = 4
-    addOde1(theModel, meshT, yLo, yUp, Ny, float(0.1))
+
+    #
+    # Nx = len(setT)-1
+    # theModel.setTidx = pyo.RangeSet(0,Nx)
+    #
+    # def meshT_init(m, k):
+    #     return setT[k]
+    # theModel.meshT = pyo.Param(theModel.setTidx, initialize=meshT_init)
+    #
+    # # Create y-meshgrid
+    # theModel.setYidx = pyo.RangeSet(0,Ny)
+    # theModel.dy = pyo.Param(initialize=(yUp - yLo)/Ny)
+    # dy = (yUp - yLo)/Ny
+    #
+    # def meshY_init(m, i):
+    #     return yLo + i*dy
+    # theModel.meshY = pyo.Param(theModel.setYidx, initialize=meshY_init)
+    #
+    # def printData():
+    #     print("||||||||||| DATA |||||||||||||||")
+    #     print("Nx = ", Nx, " Ny = ", Ny)
+    #     print("meshT: ", [t for t in setT])
+    #     print("meshY: ", [y for y in theModel.meshY])
+    #     print("||||||||||||||||||||||||||||||||")
+    # printData()
+    #
+    # theModel.Fy = pyo.Var(theModel.setYidx, within=pyo.Reals)
+    # theModel.Xt = pyo.Var(theModel.setTidx, within=pyo.Reals)
+# addOde1(model: pyo.ConcreteModel, meshT, varXt, Nx: int, meshY, varFy, yMin: float, yMax: float, Ny: int, eps: float, useEta = True)
+    initXtFy(theModel, setT, yLo, yUp, Ny)
+    addOde1(theModel, setT, theModel.Xt, Nx, theModel.meshY, theModel.Fy, yLo, yUp, Ny, float(0.1))
 
     theModel.pprint()
     # print(theModel.meshT[1], theModel.meshT[len(meshT)])
+    # quit()
 
     print("\n||||||||||||||||||||||||||||| Ode1_Sqrt |||||||||||||||||||||||||||||")
     theModel = pyo.ConcreteModel("test Ode1_Sqrt")
-    addOde1(theModel, meshT, yLo, yUp, Ny, float(0.1), useEta=False)
+    initXtFy(theModel, setT, yLo, yUp, Ny)
+    addOde1(theModel, setT, theModel.Xt, Nx, theModel.meshY, theModel.Fy, yLo, yUp, Ny, float(0.1), useEta=False)
     theModel.pprint()
