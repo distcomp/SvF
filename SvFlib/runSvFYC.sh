43cde4d49e731fe6134506d5bf66766045d8eb53
Vladimir Voloshinov <vladimir.voloshinov@gmail.com>
2023-04-23 22:05:21 +0300

    test access to git repo

--- SvFlib/runSvFYC.sh
+++ SvFlib/runSvFYC.sh
@@ -14,20 +14,5 @@ echo  "python /home/vladimirv/git_work/SvF/SvFlib/_START.py"
 #python /home/sokol/D/SvF/SvFlib/_START.py $1
 python /home/vladimirv/git_work/SvF/SvFlib/_START.py $1
 
-#exit_code=$?
-
-#echo 'exit_code:'
-#echo $exit_code
-#echo '########## RUN StartModel.py #####################', $exit_code
-
-#if [ $exit_code -eq 0 ]
-#then
-#   python StartModel.py
-#else
-#   echo "что-то пошло не так"
-#fi
-#python StartModel.py
-
-#python $SVF_HOME/Lib26/_START26n.py >> SvF.out
 
 #########################  http://citforum.ru/programming/shell/gl4.shtml    ####################
